2aadc52c21f2f9067a27d5add3c2c50b84533a14
Kabillan T A <kabillan1905@gmail.com>
2025-01-28 15:25:20 +0530

    feat: Implemented AI Inference for analysing the data for graphs

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 import streamlit as st 
+import matplotlib.pyplot as plt
+import seaborn as sns
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
@@ -20,6 +22,49 @@ def summary(df):
     chain = prompt_template | model | StrOutputParser()
     result = chain.invoke({"datasample": df.head(10).to_string(index=False)})
     return result
+
+def analysis_for_graphs(df):
+    metadata = {col: {
+        "dtype": "categorical" if df[col].dtype == "object" else "numerical",
+        "unique_values": df[col].nunique(),
+        "missing_values": df[col].isna().sum()
+    }
+    for col in df.columns
+    }
+    
+    messages = [
+    ("system", "You are an expert in Exploratory Data Analysis (EDA)."),
+    ("human", '''
+     
+    ### **Guidelines:**
+    - **Categorical Data (object, low unique counts < 30):** Use `"Bar Chart"` or `"Pie Chart"`.
+    - **Numerical Data (continuous):** Use `"Histogram"` for distribution, `"Box Plot"` for outliers.
+    - **Time Series Data:** Use `"Line Chart"`.
+    - **Correlation Analysis:** Use `"Scatter Plot"` for relationships between two numerical columns.
+    - If the column is not relevant for visualization, return `"No Visualization Needed"`.
+
+    ### **Column Names:**
+    {column_names}
+
+    ### **Metadata:**
+    {metadata}
+     
+    ### **Retunrn the out strictly in below mentioned format no extra output is required:**
+
+    ```json
+    {{
+    "column 1": "Visualization Type",
+    "column 2": "Visualization Type",
+    "column 3": "Visualization Type"
+     till 
+    "column n": "Visualization Type"
+
+    }}''')]
+    prompt_template = ChatPromptTemplate.from_messages(messages)
+    chain = prompt_template | model | StrOutputParser()
+    result = chain.invoke({'column_names':str(df.columns.to_list()),'metadata':str(metadata)})
+    return result
+
 
 def main():
     st.title("Auto EDA")
@@ -46,6 +91,7 @@ def main():
         st.write(df.shape)
         st.write("### Missing Values:")
         st.dataframe(df.isnull().sum(),use_container_width=True)
+        st.write(analysis_for_graphs(df))
     else:
         st.write("Upload a CSV file to get started!")
 
